88bad34e3bfb9deb757acebd54f01fef10d02cd4
FinzHere <finthebestintheworld13@gmail.com>
2026-07-23 10:01:22 +0100

    Added .gitattributes and .gitignote

--- Phillip Menu Temp/obj/Debug/netstandard2.1/PhillipMenu.AssemblyInfo.cs
+++ Phillip Menu Temp/obj/Debug/netstandard2.1/PhillipMenu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhillipMenu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+451d26f357806ba3815d34f5e96faf288b2d1973")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhillipMenu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhillipMenu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
